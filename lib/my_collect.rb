9fd3bf34710d45d30cd37dea02117b916e909a6d
Kyle Houghton <kylebenz@hotmail.com>
2018-12-27 20:39:39 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,7 @@ def my_collect(collection)
   return puts "No block given!" unless block_given?
   i = 0
   return_value = Array.new
-  while i < array.length
+  while i < collection.length
     return_value << yield (array[i])
     i += 1
   end
