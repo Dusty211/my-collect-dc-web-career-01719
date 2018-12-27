07a204b6eae2b7ccef1d10fb0f91a3d1b249b617
Kyle Houghton <kylebenz@hotmail.com>
2018-12-27 20:42:18 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,6 @@
 
 def my_collect(collection)
-  return puts "No block given!" unless block_given?
+  #return puts "No block given!" unless block_given?
   i = 0
   return_value = Array.new
   while i < collection.length
