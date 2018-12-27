ef953714fd7e076367100028fb8f2da944095824
Kyle Houghton <kylebenz@hotmail.com>
2018-12-27 20:26:43 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,7 @@ def my_collect(collection)
   i = 0
   return_value = Array.new
   while i < array.length
-    return_value << (yield array[i])
+    return_value << yield(array[i])
   end
   return_value
 end
